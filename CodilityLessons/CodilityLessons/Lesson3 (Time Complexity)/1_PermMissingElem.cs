db7feee7c15f25a408710549fcd55d78710352d4
drone <studmm@ukr.net>
2018-10-27 19:59:12 +0300

    solution 3.1

---
+++ CodilityLessons/CodilityLessons/Lesson3 (Time Complexity)/1_PermMissingElem.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+
+namespace CodilityLessons {
+
+	public sealed class PermMissingElem {
+
+		public int solution( int[] A ) {
+
+			long result = (long)( A.Length + 1 ) * ( A.Length + 2 ) / 2;
+
+			foreach( int item in A )
+				result -= item;
+
+			return (int)result;
+
+		}
+
+		[Theory]
+		[InlineData( 2, new[] { 1 } )]
+		[InlineData( 4, new[] { 2, 3, 1, 5 } )]
+		public void Test( int expected, int[] input ) => Assert.Equal( expected, solution( input ) );
+	}
+
+}
