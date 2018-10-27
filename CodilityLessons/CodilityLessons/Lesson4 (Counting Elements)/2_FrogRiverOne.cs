e922a1676aa7f42e1e93789de3fdf0d55bb4f6c7
drone <studmm@ukr.net>
2018-10-27 22:46:10 +0300

    solution 4.2

---
+++ CodilityLessons/CodilityLessons/Lesson4 (Counting Elements)/2_FrogRiverOne.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CodilityLessons {
+
+	public sealed class FrogRiverOne {
+
+		public int solution1( int X, int[] A ) {
+
+			var set = new HashSet<int>();
+			
+			for( var i = 0; i < A.Length; i++ ) {
+				set.Add( A[i] );
+				if( set.Count == X ) return i;
+			}
+			
+			return -1;
+		}
+
+		// A bit more efficient solution
+		public int solution2( int X, int[] A ) {
+
+			var positions = new BitArray(1 + X);
+			var acc = ( X + 1 ) * X / 2;
+
+			for( int i = 0; i < A.Length; i++ ) {
+
+				if( positions[A[i]] ) continue;
+
+				positions[A[i]] = true;
+				acc -= A[i];
+
+				if( acc == 0 ) return i;
+			}
+
+			return -1;
+		}
+
+
+		[Theory]
+		[InlineData( 6, 5, new[] { 1, 3, 1, 4, 2, 3, 5, 4 } )]
+		public void Test1( int expected, int x, int[] input ) => Assert.Equal( expected, solution1( x, input ) );
+
+		[Theory]
+		[InlineData( 6, 5, new[] { 1, 3, 1, 4, 2, 3, 5, 4 } )]
+		public void Test2( int expected, int x, int[] input ) => Assert.Equal( expected, solution2( x, input ) );
+
+	}
+}
--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Solutions to Codility Lessons in C#
 
 ### Counting Elements
 - [x] [PermCheck](https://app.codility.com/programmers/lessons/4-counting_elements/perm_check/)
-- [ ] FrogRiverOne
+- [x] [FrogRiverOne](https://app.codility.com/programmers/lessons/4-counting_elements/frog_river_one/)
 - [ ] MissingInteger
 - [ ] MaxCounters
 
